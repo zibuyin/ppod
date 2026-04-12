10eb36ca84992ce0015caec7f3564aa992fdb9ca
zibuyin <natdrone101@gmail.com>
2026-04-12 16:00:59 +0100

    fix: config permission issue

--- install.sh
+++ install.sh
@@ -3,4 +3,5 @@
 echo Setting up config file at /etc/ppod/ppod.conf
 mkdir -p /etc/ppod/
 touch /etc/ppod/ppod.conf
-mv ./ppod.conf.template /etc/ppod/ppod.conf
+cp ./ppod.conf.template /etc/ppod/ppod.conf
+sudo chmod 640 /etc/ppod/ppod.conf
